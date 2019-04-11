c282c2248b88ff6e89f934db9737c4a0d1ea038f
Samuel Pocino <43758828+spocino@users.noreply.github.com>
2019-04-11 15:15:44 -0400

    Code to read file in
    added fileinput loop to read arguments or stdin into a string

--- MD2HTML.py
+++ MD2HTML.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python
 '''
 Samuel Pocino
+Created 4/11/2019
+
+reads all files in arguments as markdown to be parsed, or stdin if no files are specified.
 '''
 
 import sys
 import fileinput
 
+mdfile = ""
 for line in fileinput.input():
-  pass
+  mdfile = mdfile + line
